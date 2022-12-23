68d2a93559d5ad37f8b4fcd45183ee63ae3b4760
Cerberus270 <geoavila718@gmail.com>
2022-12-23 12:48:07 -0600

    Fix: Modify CORS Policy to allow All Origins

--- ReactNET_API/Program.cs
+++ ReactNET_API/Program.cs
@@ -9,6 +9,12 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Connection")));
 
+builder.Services.AddCors(p => p.AddPolicy("corspolicy", build =>
+{
+    // Wildcard * allow all origins
+    build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,10 +24,7 @@ if (!app.Environment.IsDevelopment())
     app.UseHttpsRedirection();
 }
 
-app.UseForwardedHeaders(new ForwardedHeadersOptions
-{
-    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
-});
+app.UseCors("corspolicy");
 
 app.UseAuthorization();
 
